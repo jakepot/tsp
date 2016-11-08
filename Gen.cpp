704e89ae0ee4ff73632fb40135447a4ffb3a8689
Mikołaj Rozwadowski <mikolaj.rozwadowski@outlook.com>
2016-11-08 15:14:36 +0100

    Make Gen.cpp an executable with its own main()

--- Gen.cpp
+++ Gen.cpp
@@ -6,16 +6,35 @@
 #include <ctime>
 #include <vector>
 #include "Point.h"
+using namespace std;
 
 std::vector <Point> generate(int n) {
-    std::vector <Point> Points;
+    std::vector <Point> points;
 
     srand((unsigned)time(NULL));
 
     for (int i = 0; i < n; i++){
-        Points.push_back(Point(i, rand()%100, rand()%100));
-        std::cout << Points[i];
+        points.push_back(Point(i, rand()%100, rand()%100));
     }
 
-    return Points;
+    return points;
+}
+
+int main(int argc, char **argv) {
+    int n;
+    if (argc < 2) {
+        cerr << "Podaj liczbę punktów do wygenerowania jako parametr!";
+        return 1;
+    }
+    n = atoi(argv[1]); // n is set to 0 if it can't be converted
+    if (n < 2) {
+        cerr << "Należy wygenerować co najmniej dwa punkty!";
+    }
+
+    auto points = generate(n);
+    for (auto point: points) {
+        cout << point << endl;
+    }
+
+    return 0;
 }
